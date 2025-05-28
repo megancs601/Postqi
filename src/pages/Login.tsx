50940ca469383a63fa1d7cae040818cf5cdb1443
Megan C. Smith <megan.cs601@gmail.com>
2025-05-28 16:43:54 -0400

    login form with username, password and css styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+
 export default function Login() {
-  return <h1>Login Page</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(username, password);
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleLogin}
+        className="max-w-sm m-auto mt-40 grid grid-cols-1 gap-2 border-1 border-gray-800 p-6 rounded-lg dark:bg-gray-900"
+      >
+        <h1 className="m-auto mb-7 text-2xl">Log in to Postqi</h1>
+        <div className="grid grid-cols-1 relative">
+          <label htmlFor="username" className="text-slate-400 mb-2">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            placeholder="FooBar"
+            onChange={(e) => setUsername(e.target.value)}
+            className="px-2 py-1 border-1 border-gray-800 focus:outline focus:outline-sky-500 rounded-md dark:bg-gray-950 placeholder:text-gray-500"
+          />
+        </div>
+        <div className="grid grid-cols-1 mt-3 relative">
+          <label htmlFor="password" className="text-slate-400">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="123456"
+            onChange={(e) => setPassword(e.target.value)}
+            className="px-2 py-1 border-1 border-gray-800 focus:outline focus:outline-sky-500 rounded-md dark:bg-gray-950 placeholder:text-gray-500"
+          />
+        </div>
+        <button
+          type="submit"
+          className="px-3 py-2 mt-4 rounded-md bg-sky-500 hover:bg-sky-600 hover:cursor-pointer"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
