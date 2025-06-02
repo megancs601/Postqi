a428405384c5b91d6433fdda3d5797198d226be7
Megan C. Smith <megan.cs601@gmail.com>
2025-06-02 14:23:30 -0400

    minor css changes for columns and task cards to make drag animations feel/look better

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -8,13 +8,18 @@ interface ColumnProps {
 
 export default function Column({ column }: ColumnProps) {
   return (
-    <div className="min-h-screen bg-slate-900 border border-slate-700 p-3 rounded-sm">
+    <div className="h-screen flex flex-col bg-slate-900 border border-slate-700 p-3 rounded-sm">
+      <h2 className="mb-1">{column.title}</h2>
+      <hr className={`border-1 ${column.color} mb-3`} />
+
       <Droppable droppableId={column.id}>
         {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            <h2 className="mb-1">{column.title}</h2>
-            <hr className={`border-1 ${column.color} mb-3`} />
-            <div className="grid grid-cols-1 gap-y-2">
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="min-w-72 flex-1 overflow-y-auto"
+          >
+            <div className="grid grid-cols-1">
               {column.tasks.map((task, index) => (
                 <TaskCard key={task.id} task={task} index={index} />
               ))}
