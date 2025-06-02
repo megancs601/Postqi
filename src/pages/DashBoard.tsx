8c2b48ce26de0e20589b0f81e06afd409bcd8dcf
Megan C. Smith <megan.cs601@gmail.com>
2025-06-02 14:22:31 -0400

    dragEnd functionality for updating columns when tasks are moved around

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,16 +1,42 @@
 import { useState } from "react";
 import type { ColumnState } from "../types/board";
 import { initialData } from "../data/initialState";
-import { DragDropContext } from "@hello-pangea/dnd";
+import { DragDropContext, type DropResult } from "@hello-pangea/dnd";
 import Column from "../components/Column";
 
 export default function Dashboard() {
   const [columns, setColumns] = useState<ColumnState>(initialData);
 
-  console.log(columns);
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+    // if no new destination, return early
+    if (!destination) return;
 
-  const onDragEnd = () => {
-    console.log("on drag end");
+    const sourceCol = columns[source.droppableId];
+    const sourceTasks = [...sourceCol.tasks];
+    const [movedTask] = sourceTasks.splice(source.index, 1);
+
+    // if same column, get selected task and move it to new index of same column, update data
+    if (source.droppableId == destination.droppableId) {
+      sourceTasks.splice(destination.index, 0, movedTask);
+      setColumns({
+        ...columns,
+        [sourceCol.id]: { ...sourceCol, tasks: sourceTasks },
+      });
+
+      return;
+    }
+
+    // else get new column id, and insert selected task at new index, update data
+    const destinationCol = columns[destination.droppableId];
+    const destinationTasks = [...destinationCol.tasks];
+
+    destinationTasks.splice(destination.index, 0, movedTask);
+    setColumns({
+      ...columns,
+      [sourceCol.id]: { ...sourceCol, tasks: sourceTasks },
+      [destinationCol.id]: { ...destinationCol, tasks: destinationTasks },
+    });
   };
 
   return (
