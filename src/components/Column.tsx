b2fc1dfdebbd023746fefac4aed35d5f13e7de99
Megan C. Smith <megan.cs601@gmail.com>
2025-06-03 11:24:43 -0400

    added add button

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -9,8 +9,17 @@ interface ColumnProps {
 export default function Column({ column }: ColumnProps) {
   return (
     <div className="h-screen flex flex-col bg-slate-900 border border-slate-700 p-3 rounded-sm">
-      <h2 className="mb-1">{column.title}</h2>
-      <hr className={`border-1 ${column.color} mb-3`} />
+      <div className="flex flex-row justify-between">
+        <h2 className="text-slate-300 h-min">{column.title}</h2>
+        {column.id === "todo" && (
+          <button className="hover:cursor-pointer hover:bg-slate-700 px-1 h-6 rounded-sm">
+            <span className="material-symbols-outlined  text-slate-300">
+              add
+            </span>
+          </button>
+        )}
+      </div>
+      <hr className={`border-1 ${column.color} mt-1  mb-3 `} />
 
       <Droppable droppableId={column.id}>
         {(provided) => (
