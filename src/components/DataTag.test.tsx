d7d2bcd6af914e14ba733a760ac7b15243213038
Megan C. Smith <megan.cs601@gmail.com>
2025-06-11 20:47:18 -0400

    finished up date tag tests

--- src/components/DataTag.test.tsx
+++ src/components/DataTag.test.tsx
@@ -8,3 +8,18 @@ it("shows a formatted date", () => {
   const time = screen.getByLabelText(/due/i);
   expect(time).toHaveTextContent("10/30/2022");
 });
+
+it("shows correct styles for overdue date", () => {
+  render(<DateTag date={fakeDate} />);
+  const time = screen.getByLabelText(/due/i);
+
+  expect(time).toHaveClass(/bg-red/i);
+});
+
+it("shows correct styles for a future date", () => {
+  render(<DateTag date="3000-03-10T00:00:00Z" />);
+  const time = screen.getByLabelText(/due/i);
+
+  expect(time).not.toHaveClass(/bg-red/i);
+  expect(time).toHaveClass(/text-gray/i);
+});
