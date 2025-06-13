b5afb2d53191d231910eeef9fe51cd63f080dacc
Megan C. Smith <megan.cs601@gmail.com>
2025-06-13 13:00:31 -0400

    added component test for Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import Login from "./pages/Login";
 import Dashboard from "./pages/DashBoard";
 import About from "./pages/About";
@@ -19,28 +19,23 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login onLogin={validAuthentication} />} />
-        <Route
-          path="/login"
-          element={<Login onLogin={validAuthentication} />}
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/*"
-          element={<NotFound isAuthenticated={isAuthenticated} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login onLogin={validAuthentication} />} />
+      <Route path="/login" element={<Login onLogin={validAuthentication} />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/about" element={<About />} />
+      <Route
+        path="/*"
+        element={<NotFound isAuthenticated={isAuthenticated} />}
+      />
+    </Routes>
   );
 }
 
