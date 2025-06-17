7c1e7ad86acc6d92dd492c9dabbb2d687e18b138
Megan C. Smith <megan.cs601@gmail.com>
2025-06-17 12:17:49 -0400

    finished TaskCardAction tests

--- src/components/TaskCardAction.test.tsx
+++ src/components/TaskCardAction.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../store/slices/authSlice";
@@ -7,6 +7,7 @@ import TaskCardAction from "./TaskCardActions";
 import * as boardSlice from "../store/slices/boardSlice";
 import userEvent from "@testing-library/user-event";
 
+const user = userEvent.setup();
 const columns = [
   {
     id: "test-col-1",
@@ -39,7 +40,6 @@ const columns = [
   { id: "test-col-2", title: "Test Column 2", tasks: [], color: "green" },
 ];
 
-// Required drag context for <Droppable>
 const renderWithStore = (ui: React.ReactNode) => {
   const store = configureStore({
     reducer: { auth: authReducer, board: boardReducer },
@@ -58,7 +58,6 @@ afterEach(() => {
 
 it("has expected behavior when 'move up' is clicked", async () => {
   const moveTaskSpy = vi.spyOn(boardSlice, "moveTask");
-  const user = userEvent.setup();
 
   renderWithStore(
     <TaskCardAction
@@ -68,7 +67,6 @@ it("has expected behavior when 'move up' is clicked", async () => {
     />,
   );
 
-  // up
   await user.click(screen.getByText("more_horiz"));
   await screen.findByText("Task Actions");
 
@@ -83,7 +81,6 @@ it("has expected behavior when 'move up' is clicked", async () => {
 
 it("has expected behavior when 'move down' is clicked", async () => {
   const moveTaskSpy = vi.spyOn(boardSlice, "moveTask");
-  const user = userEvent.setup();
 
   renderWithStore(
     <TaskCardAction
@@ -102,5 +99,52 @@ it("has expected behavior when 'move down' is clicked", async () => {
     destinationColId: "test-col-1",
     sourceIndex: 2,
     destinationIndex: 3,
+  });
+});
+
+it("has expected behavior when 'move to column' is clicked", async () => {
+  const moveTaskSpy = vi.spyOn(boardSlice, "moveTask");
+
+  renderWithStore(
+    <TaskCardAction
+      index={2}
+      columnId={columns[0].id}
+      taskId={columns[0].tasks[0].id}
+    />,
+  );
+  await user.click(screen.getByText("more_horiz"));
+  await screen.findByText("Task Actions");
+
+  // open submenu
+  await user.click(screen.getByText(/move to column/i));
+
+  // user.click() wasnt working, fireEvent.click() is best used with custom UI libraries
+  fireEvent.click(await screen.findByText(/test column 2/i));
+
+  expect(moveTaskSpy).toHaveBeenCalledWith({
+    sourceColId: "test-col-1",
+    destinationColId: "test-col-2",
+    sourceIndex: 2,
+    destinationIndex: 0, // since test-col-2 is empty
+  });
+});
+
+it("has expected behavior when 'delete' is clicked", async () => {
+  const deleteTaskSpy = vi.spyOn(boardSlice, "deleteTask");
+
+  renderWithStore(
+    <TaskCardAction
+      index={2}
+      columnId={columns[0].id}
+      taskId={columns[0].tasks[0].id}
+    />,
+  );
+  await user.click(screen.getByText("more_horiz"));
+  await screen.findByText("Task Actions");
+
+  await user.click(screen.getByText(/delete/i));
+  expect(deleteTaskSpy).toHaveBeenCalledWith({
+    columnId: "test-col-1",
+    taskId: "fake-task-1",
   });
 });
