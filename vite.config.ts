af8c5aac65f2fc66ea37de7adbbcdf6c4ddb4039
Megan C. Smith <megan.cs601@gmail.com>
2025-06-18 14:25:58 -0400

    added temporary favicon, updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/postqi/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/postqi/" : "/",
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
   },
-});
+}));
