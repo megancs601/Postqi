a53d9ad614b7133cf0581a64238ae3f4f85751e9
Megan C. Smith <megan.cs601@gmail.com>
2025-06-24 19:26:43 -0400

    added new tests for changing task priority

--- src/components/PriorityTag.test.tsx
+++ src/components/PriorityTag.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import { renderWithStore } from "../utils/TestUtils";
 import PriorityTag from "./PriorityTag";
+import userEvent from "@testing-library/user-event";
 
 it("renders correct icon and label for high priority", () => {
   renderWithStore({
@@ -30,4 +31,31 @@ it("renders correct icon and label for low priority", () => {
 
   const tag = screen.getByText("keyboard_arrow_down");
   expect(tag.className).toMatch(/text-blue/i);
+});
+
+it("opens the change task priority menu when the priorty icon is clicked", async () => {
+  renderWithStore({
+    ui: <PriorityTag priority={2} taskId="fakeTestId" columnId="fakeColId" />,
+  });
+  const user = userEvent.setup();
+  await user.click(screen.getByLabelText(/medium priority/i));
+
+  await screen.findByText(/change task priority/i);
+
+  screen.getByRole("menuitem", { name: /high/i });
+  screen.getByRole("menuitem", { name: /medium/i });
+  screen.getByRole("menuitem", { name: /low/i });
+});
+
+it("renders correct priorty icon when new priority  is selected", async () => {
+  renderWithStore({
+    ui: <PriorityTag priority={3} taskId="fakeTestId" columnId="fakeColId" />,
+  });
+  const user = userEvent.setup();
+  await user.click(screen.getByLabelText(/low priority/i));
+
+  await screen.findByText(/change task priority/i);
+  await user.click(screen.getByRole("menuitem", { name: /high/i }));
+
+  screen.findByLabelText(/high priority/i);
 });
